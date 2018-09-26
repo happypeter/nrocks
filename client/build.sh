a7d803e792826823078fa95579dc5bc6bb8b7177
Billie Zhang <billiecoder@gmail.com>
2018-09-26 16:30:01 +0800

    upload static files to server with script

--- client/build.sh
+++ client/build.sh
@@ -1,3 +1,9 @@
 rm -rf dist
 yarn build
-yarn serve
+SSHPASS='xxx' sshpass -e scp -r dist user@host:~/nrocks/next
+SSHPASS='xxx' sshpass -e ssh host <<EOF
+  cd nrocks
+  rm -rf dist
+  mv next dist
+  exit
+EOF
