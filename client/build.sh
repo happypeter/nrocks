82be7ec0082b106adae82c2648740700e6a37234
Billie Zhang <billiecoder@gmail.com>
2018-10-26 21:35:15 +0800

    rm build.sh

--- client/build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf dist
-yarn build
-SSHPASS='xxx' sshpass -e scp -r dist user@host:~/nrocks/next
-SSHPASS='xxx' sshpass -e ssh host <<EOF
-  cd nrocks
-  rm -rf dist
-  mv next dist
-  exit
-EOF
