097e63e026c4d0cff3f3246ba8ee741580a1d549
Billie Zhang <billiecoder@gmail.com>
2019-03-06 09:57:33 +0800

    remove source code

--- client/config.default.js
+++
@@ -1,4 +0,0 @@
-export default {
-  docRepo: '/path/to/ncourse',
-  gitHubRepo: 'https://github.com/happypeter/ncourse/blob/master'
-}
--- client/src/containers/CourseContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { withRouteData } from 'react-static'
-import Course from '../components/Course'
-
-const CourseContainer = props => <Course {...props} />
-
-export default withRouteData(CourseContainer)
