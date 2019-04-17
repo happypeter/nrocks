959931948974e6bef35400b6118dd6fe33e1f8d7
Billie Zhang <billiecoder@gmail.com>
2019-04-17 13:40:46 +0800

    change github repo address

--- src/site-constants.js
+++ src/site-constants.js
@@ -1,5 +1,5 @@
 // Code repo address on GitHub
-const gitHubRepo = 'https://github.com/happypeter/nrocks/blob/master';
+const gitHubRepo = 'https://github.com/nervos-community/nrocks/tree/master';
 
 // NOTE: We can't just use `location.toString()` because when we are rendering
 // the SSR part in node.js we won't have a proper location.
